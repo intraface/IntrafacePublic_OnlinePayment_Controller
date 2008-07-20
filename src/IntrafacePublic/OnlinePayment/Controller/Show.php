28e4ad9f46de0d265b3b6d1d6e3ce3bad378a206
Lars Olesen <lars@intraface.dk>
2008-07-20 07:05:18 +0000

    Changed the templates a bit.

--- src/IntrafacePublic/OnlinePayment/Controller/Show.php
+++ src/IntrafacePublic/OnlinePayment/Controller/Show.php
@@ -1,16 +1,7 @@
 <?php
-
 class IntrafacePublic_OnlinePayment_Controller_Show extends k_Controller
 {
     private $error = array();
-
-    function __construct($context, $name)
-    {
-        parent::__construct($context, $name);
-
-        $GLOBALS['_global_function_callback_e'] = Array($this, 'outputString');
-        $GLOBALS['_global_function_callback_url'] = Array($this, 'url');
-    }
 
     function GET()
     {
@@ -31,9 +22,8 @@ class IntrafacePublic_OnlinePayment_Controller_Show extends k_Controller
             $this->url('./postprocess'),
             $this->url('./input'));
         
-        
         // if no slashes in destination it is local.
-        if(!strpos($prepare->getPostDestination(), '/')) {
+        if (!strpos($prepare->getPostDestination(), '/')) {
             $destination = $this->url($prepare->getPostDestination());
         } else {
             $destination = $prepare->getPostDestination();
@@ -46,7 +36,7 @@ class IntrafacePublic_OnlinePayment_Controller_Show extends k_Controller
         $data['payment_provider'] = $prepare->getProviderName();
         $data['error'] = $this->error;
 
-        if(!empty($this->GET['error'])) {
+        if (!empty($this->GET['error'])) {
             return $forward = $this->render('IntrafacePublic/OnlinePayment/templates/payment-error-tpl.php', $data);
         } else {
             return $forward = $this->render('IntrafacePublic/OnlinePayment/templates/payment-forward-tpl.php', $data);
@@ -55,37 +45,29 @@ class IntrafacePublic_OnlinePayment_Controller_Show extends k_Controller
     
     public function forward($name) 
     {
-        /**
-         * The provider payment server makes an http post call to the postprocess page.
-         * This page then adds the payment to intraface.
-         * 
-         */
-        if($name == 'postprocess') {
+        // The provider payment server makes an http post call to the postprocess page.
+        // This page then adds the payment to intraface.
+        if ($name == 'postprocess') {
             $next = new IntrafacePublic_OnlinePayment_Controller_PostProcess($this, $name);
             return $next->handleRequest();
         }
         
-        /**
-         * The input page provides html input fields for the provider payment server. 
-         * The server gets the fields through a http call.
-         */
-        if($name == 'input') {
+        // The input page provides html input fields for the provider payment server. 
+        // The server gets the fields through a http call.
+        if ($name == 'input') {
             $next = new IntrafacePublic_OnlinePayment_Controller_Input($this, $name);
             return $next->handleRequest();
         }
         
-        /**
-         * Returns a confirmation that the payment was successfull
-         */
-        if($name == 'receipt') {
+        // Returns a confirmation that the payment was successfull
+        if ($name == 'receipt') {
             $next = new IntrafacePublic_OnlinePayment_Controller_Receipt($this, $name);
             return $next->handleRequest();
         }
         
-        
         $prepare = $this->registry->get('onlinepayment:payment_html')->getPrepare();
         // if there is no slashes in post destination it must be a local server.
-        if(!strpos('/', $prepare->getPostDestination()) && $name == $prepare->getPostDestination()) {
+        if (!strpos('/', $prepare->getPostDestination()) && $name == $prepare->getPostDestination()) {
             $next = new Ilib_Payment_Html_Controller_Server($this, $name);
             return $next->handleRequest();
         }
--- src/IntrafacePublic/OnlinePayment/templates/payment-forward-tpl.php
+++ src/IntrafacePublic/OnlinePayment/templates/payment-forward-tpl.php
@@ -1,16 +1,13 @@
-
-
 <div id="payment-forward">
-    <p><?php e(__('You are now ready to pay your '.$target_type.' of')); ?> <strong><?php echo $total_price; ?></strong> kr.</p>
+    <p><?php e(__('You are now ready to pay your '.$target_type.' of')); ?> <strong>DKK <?php e($total_price); ?></strong>.</p>
 
-    <p><?php e(__('The payment is made through a secure payment server provided by')); ?> <?php echo $payment_provider; ?></p>
+    <p><?php e(__('The payment is made through a secure payment server provided by')); ?> <?php e($payment_provider); ?></p>
     
-    <form method="POST" action="<?php echo $post_action; ?>">
+    <form method="POST" action="<?php e($post_action); ?>">
     
-    <?php echo $input_fields; ?>
+        <?php echo $input_fields; ?>
     
-    <p><input type="submit" name="pay" value="<?php e(__('Continue to payment...')); ?>" /></p>
+        <p id="purchase-continue"><input id="buy" type="submit" name="pay" value="<?php e(t('Continue')); ?>" /></p>
     
     </form>
-    
 </div>
